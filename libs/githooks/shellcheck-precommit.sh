2846d72a79afe0f15e8c22933d8c68801686bd56
Ubuntu <azureuser@practice-devops-vm.om2tktlnxdhuxeqgg0pcfqcr4d.rx.internal.cloudapp.net>
2025-05-20 17:18:25 +0000

    test commit after adding hooks

--- libs/githooks/shellcheck-precommit.sh
+++ libs/githooks/shellcheck-precommit.sh
@@ -6,7 +6,7 @@ else
     SHELL_CHECK_VERSION="shellcheck-v0.10.0"
     SHELL_CHECK_BINARY="/tmp/${SHELL_CHECK_VERSION}/shellcheck"
 
-    if [ ! -f $SHELL_CHECK_BINARY ]; then
+    if [[ ! -f $SHELL_CHECK_BINARY ]]; then
        wget -q https://github.com/koalaman/shellcheck/releases/download/v0.10.0/${SHELL_CHECK_VERSION}.linux.x86_64.tar.xz -O /tmp/${SHELL_CHECK_VERSION}.tar.xz
        tar -xvf /tmp/${SHELL_CHECK_VERSION}.tar.xz -C /tmp > /dev/null
        rm -rf /tmp/${SHELL_CHECK_VERSION}.tar.xz
@@ -15,10 +15,10 @@ fi
 
 SHELL_FILES=$(git diff --cached --name-only | grep -E '\.sh$')
 
-if [ -n "${SHELL_FILES}" ]; then
+if [[ -n "${SHELL_FILES}" ]]; then
   $SHELL_CHECK_BINARY $(echo $SHELL_FILES) --sevirity=warning 
 
-    if [ $? -ne 0 ]; then
+    if [[ $? -ne 0 ]]; then
         echo "Shellcheck failed. Please fix the issues before committing."
         exit 1
     fi
